915fb61a908382c83270551758e68b8cc0fd5bda
Martastain <martas@imm.cz>
2024-03-21 14:39:21 +0100

    simplified makefile (auto-extract version, .env file)

--- services/service/__main__.py
+++ services/service/__main__.py
@@ -4,44 +4,56 @@ import time
 
 import ayon_api
 
+
 SENDER = f"example-processor-{socket.gethostname()}"
+
+
+def process_event():
+    job = ayon_api.enroll_event_job(
+        source_topic="entity.folder.status_changed",
+        target_topic="example.approval_handler",
+        sender=SENDER,
+        description="Approved folder detected. Thinking...",
+        max_retries=3,
+        events_filter={
+            "conditions": [
+                {
+                    "key": "payload/newValue",
+                    "value": "Approved",
+                }
+            ]
+        }
+    )
+
+    if not job:
+        time.sleep(5)
+        return
+
+    src_job = ayon_api.get_event(job["dependsOn"])
+    ayon_project_name = src_job["project"]
+
+    ayon_api.update_event(
+        job["id"],
+        sender=SENDER,
+        status="in_progress",
+        project_name=ayon_project_name,
+        description="Stand by. I am pretending to do something...",
+    )
+
+    time.sleep(2)
+
+    ayon_api.update_event(
+        job["id"],
+        sender=SENDER,
+        status="finished",
+        project_name=ayon_project_name,
+        description=f"Good job {src_job['user']}! Your folder has been approved.",
+    )
+
 
 def main():
     while True:
-        job = ayon_api.enroll_event_job(
-            source_topic="entity.folder.status_changed",
-            target_topic="my_cool_status_changed_event",
-            sender=SENDER,
-            description="someone changed something",
-            max_retries=3,
-        )
-
-        if not job:
-            time.sleep(5)
-            continue
-
-        src_job = ayon_api.get_event(job["dependsOn"])
-        ayon_project_name = src_job["project"]
-
-        ayon_api.update_event(
-            job["id"],
-            sender=SENDER,
-            status="in_progress",
-            project_name=ayon_project_name,
-            description=f"doing some work on entity which was {src_job['payload']['newValue']}...",
-        )
-
-        time.sleep(10)
-
-        ayon_api.update_event(
-            job["id"],
-            sender=SENDER,
-            status="finished",
-            project_name=ayon_project_name,
-            description="My job is done!",
-        )
-
-
+        process_event()
 
 
 if __name__ == "__main__":
