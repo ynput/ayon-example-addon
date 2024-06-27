16bf904fb7f2e63c2195aa3a370afa6a5394f6eb
Innders <49156310+Innders@users.noreply.github.com>
2024-06-27 10:07:45 +0100

    feat: add more actions

--- server/actions.py
+++ server/actions.py
@@ -3,25 +3,87 @@ from ayon_server.actions import SimpleActionManifest
 
 EXAMPLE_SIMPLE_ACTIONS = [
     SimpleActionManifest(
-        identifier="example-folder-action",
-        label="Example Folder Action",
-        category="Server",
+        identifier="folder-action",
+        label="Folder Action",
+        category="server",
         order=100,
-        icon="{addon_url}/public/icons/maya.png",
 
         entity_type="folder",
         entity_subtypes=None,
         allow_multiselection=False,
     ),
     SimpleActionManifest(
-        identifier="example-task-action",
-        label="Example Task Action",
-        category="Launcher",
+        identifier="folder-action-2",
+        label="Folder Do Stuff",
+        category="server",
+        order=100,
+
+        entity_type="folder",
+        entity_subtypes=None,
+        allow_multiselection=False,
+    ),
+    SimpleActionManifest(
+        identifier="folder-action-2",
+        label="Delete folder",
+        category="admin",
+        order=100,
+
+        entity_type="folder",
+        entity_subtypes=None,
+        allow_multiselection=False,
+    ),
+    SimpleActionManifest(
+        identifier="task-action",
+        label="Task Action",
+        category="server",
+        order=100,
+
+        entity_type="task",
+        entity_subtypes=None,
+        allow_multiselection=False,
+    ),
+    SimpleActionManifest(
+        identifier="launch-nuke",
+        label="Launch Nuke",
+        category="application",
         order=100,
         icon="{addon_url}/public/icons/nuke.png",
 
         entity_type="task",
-        entity_subtypes=None,
+        entity_subtypes=["compositing", "roto", "matchmove"],
         allow_multiselection=True,
+    ),
+    SimpleActionManifest(
+        identifier="launch-photoshop",
+        label="Launch Photoshop",
+        category="application",
+        order=100,
+        icon="{addon_url}/public/icons/photoshop.png",
+
+        entity_type="task",
+        entity_subtypes=["compositing", "texture"],
+        allow_multiselection=True,
+    ),
+    SimpleActionManifest(
+        identifier="launch-houdini",
+        label="Launch Houdini",
+        category="application",
+        order=100,
+        icon="{addon_url}/public/icons/houdini.png",
+
+        entity_type="task",
+        entity_subtypes=["fx", "modeling"],
+        allow_multiselection=True,
+    ),
+    SimpleActionManifest(
+        identifier="launch-maya",
+        label="Launch Maya",
+        category="application",
+        order=100,
+        icon="{addon_url}/public/icons/houdini.png",
+
+        entity_type="task",
+        entity_subtypes=["fx", "modeling", "lighting", "animation", "rigging", "lookdev"],
+        allow_multiselection=False,
     )
 ]
